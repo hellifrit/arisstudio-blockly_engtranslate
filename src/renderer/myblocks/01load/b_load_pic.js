1fdfe857c89a9a38c1829840cc8d8be2301508d3
hellifrit <hellifrit@gmail.com>
2026-06-06 14:03:30 +0800

    Update b_load_pic.js

--- src/renderer/myblocks/01load/b_load_pic.js
+++ src/renderer/myblocks/01load/b_load_pic.js
@@ -2,20 +2,20 @@ import * as Blockly from 'blockly/core';
 import { javascriptGenerator } from 'blockly/javascript';
 import myLoader from 'renderer/models/loadcmd'
 import { wrapStr } from 'renderer/utils/DataTool';
-// 定义JSON格式自定义模块
+// Define custom module in JSON format
 let blockname="b_load_pic"
-// 带有映射的学生名
+// Match Student's name
 const jsondesc = {
     "type": `${blockname}`,
-    "message0": "加载%1图片 昵称 %2 文件名 %3",
+    "message0": "Load%1Image Nickname %2 Filename %3",
     "args0": [
         {
             "type": "field_dropdown",
             "name": "drop1",
             "options": [
-                ["背景","bg"],
-                ["中景","mg"],
-                ["前景","fg"],
+                ["Background","bg"],
+                ["Mid-ground","mg"],
+                ["Foreground","fg"],
             ]
         },
         {
@@ -33,18 +33,18 @@ const jsondesc = {
     "previousStatement": null,
     "nextStatement": null,
     "colour": 230,
-    "tooltip": "前景图片文件需要放在 /data/image/foreground 文件夹中\n中景图片文件需要放在 /data/image/midground 文件夹中\n背景图片文件需要放在 /data/image/background 文件夹中",
+    "tooltip": "Foreground image file is placed in /data/image/foreground Folder\nMid-ground image file is placed in /data/image/midground Folder\nBackground image file is placed in /data/image/background Folder",
     "helpUrl": ""
   }
 
-// 注入自定义模块
+// Insert Custom Module
 Blockly.Blocks[blockname] = {
     init: function () {
         this.jsonInit(jsondesc);
     }
 }
 
-// 为自定义块添加js语言生成器
+// Add JavaScript to a custom block
 javascriptGenerator[blockname] = function (block) {
     const type = block.getFieldValue('drop1');
     const nickname = javascriptGenerator.valueToCode(block, 'val1', javascriptGenerator.ORDER_ATOMIC);
